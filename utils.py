f31c699e3b2ac0bae337e7e1349b9b3937d9eff2
Paulina Szulc <paulinaszulc23@interia.pl>
2025-05-14 15:33:29 +0200

    Add method docstring

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 def add(a: int, b: int) -> int:
+    """Add two numbers."""
     return a + b
 
 def subtract(a: int, b: int) -> int:
+    """Subtract two numbers."""
     return a - b
 
 def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
     return a * b
 
 def divide(a: int, b: int) -> float:
+    """Divide two numbers."""
     return a / b
