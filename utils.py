760130fe628bb1fcfd88c30af7b8fb2ad66e7893
Paulina Szulc <paulinaszulc23@interia.pl>
2025-05-14 15:39:16 +0200

    Add black formating

--- utils.py
+++ utils.py
@@ -1,15 +1,20 @@
 """Module with basic calculator functions: add, subtract, multiply, divide."""
+
+
 def add(a: int, b: int) -> int:
     """Add two numbers."""
     return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """Subtract two numbers."""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers."""
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Divide two numbers."""
